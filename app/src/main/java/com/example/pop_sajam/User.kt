dde8217affbade66a0c9237df8ac833b5019e7f6
josip <jfuduric@foi.hr>
2019-11-18 17:42:57 +0100

    Dodana klasa User koja sadrži metode za dohvaćanje imena (korisničkog imena) i statusa konekcije.

---
+++ app/src/main/java/com/example/pop_sajam/User.kt
@@ -0,0 +1,11 @@
+package com.example.pop_sajam
+
+import com.google.gson.annotations.SerializedName
+
+class User {
+    @SerializedName("odgovor")
+    val response: String? = null
+
+    @SerializedName("ime")
+    val name: String? = null
+}
