06373b718a1111a75219225f228fdac416865a67
josip <jfuduric@foi.hr>
2019-11-18 19:33:46 +0100

    Nes nije dobro u mainactivity

--- app/src/main/java/com/example/pop_sajam/MainActivity.kt
+++ app/src/main/java/com/example/pop_sajam/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.pop_sajam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        prefConfig = PrefConfigv2(this)
+
+        if (findViewById<View>(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return
+            }
+            if (prefConfig.readLoginStatus()) {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, WelcomeFragment()).commit()
+            } else {
+                supportFragmentManager.beginTransaction().add(R.id.fragment_container, Prijava())
+                    .commit()
+            }
+        }
     }
+
+    companion object {
+        lateinit var prefConfig:PrefConfigv2;
+    }
+
 }
